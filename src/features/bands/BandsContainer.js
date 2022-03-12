805fe8ce73daa6cc8a8177f1bcbb757b9d14621e
gizem <gizemc.deniz@gmail.com>
2022-03-12 19:59:18 +0300

    done

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,22 @@
-import React from "react";
-
+import React,{useState} from "react";
+import {useDispatch,useSelector} from "react-redux"
+import BandInput from "./BandInput";
+import { bandAdded } from "./bandsSlice";
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+const bands=useSelector(state=>state.bands.entities)
+const dispatch =useDispatch()
+const onBandSubmit=(name)=>{
+  dispatch(bandAdded(name))
+}
+  return <div>BandsContainer
+
+    <BandInput  onBandSubmit={onBandSubmit}/>
+      <ul>
+        {bands.map((band) => (
+          <li key={band}>{band}</li>
+        ))}
+        </ul>
+  </div>;
 }
 
 export default BandsContainer;
--- src/features/bands/bandsSlice.js
+++ src/features/bands/bandsSlice.js
@@ -1,15 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const bandsSlice = createSlice({
   name: "bands",
   initialState: {
     entities: [],
   },
   reducers: {
+    bandAdded(state,action){
+    state.entities.push(action.payload)
+}
     // create reducer methods
   },
 });
 
-// export the action creators
-
+// export the action create
+export const {bandAdded} = bandsSlice.actions
 export default bandsSlice.reducer;
